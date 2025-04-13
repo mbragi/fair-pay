12e28a04b33dba1c8fe57f44275d5f1bbb6c38f2
Anuoluwapo25 <anuoluwapoali25@gmail.com>
2025-04-13 22:35:08 +0100

    Deployment script

--- Contract/script/DeployFairPlay.s.sol
+++ Contract/script/DeployFairPlay.s.sol
@@ -3,8 +3,9 @@ pragma solidity ^0.8.19;
 
 import "forge-std/Script.sol";
 import "../src/FairPay.sol";
+import "../src/JobEscrow.sol";
 
-contract DeployFairPay is Script {
+contract DeployFairPaySystem is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         
@@ -12,7 +13,39 @@ contract DeployFairPay is Script {
         
         FairPay fairPay = new FairPay();
         
-        console.log("FairPay deployed at:", address(fairPay));
+        fairPay.createOrganization("Test Organization", "A test organization");
+        
+        uint256 orgId = 1;
+        string memory title = "Product Manager";
+        string memory description = "Manage Product";
+        uint256 totalPayment = 1 ether;
+        uint256 milestoneCount = 3;
+        address token = address(0); 
+        
+        fairPay.createJob(
+            orgId,
+            title,
+            description,
+            totalPayment,
+            milestoneCount,
+            token
+        );
+        
+        address platform = address(fairPay);
+        address employer = vm.addr(deployerPrivateKey); 
+        uint256 platformFee = 250; 
+        
+        new JobEscrow(
+            platform,
+            employer,
+            orgId,
+            "Standalone Job",
+            "A standalone job escrow instance",
+            totalPayment,
+            milestoneCount,
+            token,
+            platformFee
+        );
         
         vm.stopBroadcast();
     }
